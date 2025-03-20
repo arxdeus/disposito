b4c42471f4dabef91da61de0ec4a87bd799ca46d
arxdeus <git@arxdeus.dev>
2025-03-20 12:16:17 +0300

    chore(extensions): change variable input name

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -4,36 +4,36 @@ import 'package:disposito/src/interfaces.dart';
 import 'package:disposito/src/internal/internal.dart';
 
 extension StreamSubscrptionExtensions<T, S extends StreamSubscription<T>> on S {
-  S bindDisposeTo(CustomDisposableCallHost disposableHolder) =>
-      disposableHolder.customDisposable(this, dispose: () => unawaited(cancel()));
+  S bindDisposeTo(CustomDisposableCallHost disposeHolder) =>
+      disposeHolder.customDisposable(this, dispose: () => unawaited(cancel()));
 }
 
 extension StreamControllerExtensions<T, S extends StreamController<T>> on S {
-  S bindDisposeTo(CustomDisposableCallHost disposableHolder) =>
-      disposableHolder.customDisposable(this, dispose: () => unawaited(close()));
+  S bindDisposeTo(CustomDisposableCallHost disposeHolder) =>
+      disposeHolder.customDisposable(this, dispose: () => unawaited(close()));
 }
 
 extension TimerExtensions on Timer {
-  Timer bindDisposeTo(CustomDisposableCallHost disposableHolder) =>
-      disposableHolder.customDisposable(this, dispose: cancel);
+  Timer bindDisposeTo(CustomDisposableCallHost disposeHolder) =>
+      disposeHolder.customDisposable(this, dispose: cancel);
 }
 
 extension SinkExtension<T, S extends Sink<T>> on S {
-  S bindDisposeTo(CustomDisposableCallHost disposableHolder) =>
-      disposableHolder.customDisposable(this, dispose: close);
+  S bindDisposeTo(CustomDisposableCallHost disposeHolder) =>
+      disposeHolder.customDisposable(this, dispose: close);
 }
 
 extension ClosableExtension<C extends Closable> on C {
-  C bindDisposeTo(CustomDisposableCallHost disposableHolder) =>
-      disposableHolder.customDisposable(this, dispose: close);
+  C bindDisposeTo(CustomDisposableCallHost disposeHolder) =>
+      disposeHolder.customDisposable(this, dispose: close);
 }
 
 extension CancelableExtension<C extends Cancelable> on C {
-  C bindDisposeTo(CustomDisposableCallHost disposableHolder) =>
-      disposableHolder.customDisposable(this, dispose: cancel);
+  C bindDisposeTo(CustomDisposableCallHost disposeHolder) =>
+      disposeHolder.customDisposable(this, dispose: cancel);
 }
 
 extension DisposableExtension<D extends Disposable> on D {
-  D bindDisposeTo(CustomDisposableCallHost disposableHolder) =>
-      disposableHolder.customDisposable(this, dispose: dispose);
+  D bindDisposeTo(CustomDisposableCallHost disposeHolder) =>
+      disposeHolder.customDisposable(this, dispose: dispose);
 }
