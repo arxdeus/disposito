11f03a4294b3cfddb398c1f04ab4f06cd7066747
arxdeus <git@arxdeus.dev>
2025-03-21 17:29:55 +0300

    fix(extensions): update extensions to new syntax

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -5,35 +5,35 @@ import 'package:disposito/src/internal/internal.dart';
 
 extension StreamSubscrptionExtensions<T, S extends StreamSubscription<T>> on S {
   S bindDisposeTo(CustomDisposableCallHost disposeHolder) =>
-      disposeHolder.bindDisposable(this, dispose: () => unawaited(cancel()));
+      disposeHolder.bindDisposable(this, dispose: (_) => unawaited(cancel()));
 }
 
 extension StreamControllerExtensions<T, S extends StreamController<T>> on S {
   S bindDisposeTo(CustomDisposableCallHost disposeHolder) =>
-      disposeHolder.bindDisposable(this, dispose: () => unawaited(close()));
+      disposeHolder.bindDisposable(this, dispose: (_) => unawaited(close()));
 }
 
 extension TimerExtensions on Timer {
   Timer bindDisposeTo(CustomDisposableCallHost disposeHolder) =>
-      disposeHolder.bindDisposable(this, dispose: cancel);
+      disposeHolder.bindDisposable(this, dispose: (_) => cancel());
 }
 
 extension SinkExtension<T, S extends Sink<T>> on S {
   S bindDisposeTo(CustomDisposableCallHost disposeHolder) =>
-      disposeHolder.bindDisposable(this, dispose: close);
+      disposeHolder.bindDisposable(this, dispose: (_) => close());
 }
 
 extension ClosableExtension<C extends Closable> on C {
   C bindDisposeTo(CustomDisposableCallHost disposeHolder) =>
-      disposeHolder.bindDisposable(this, dispose: close);
+      disposeHolder.bindDisposable(this, dispose: (_) => close());
 }
 
 extension CancelableExtension<C extends Cancelable> on C {
   C bindDisposeTo(CustomDisposableCallHost disposeHolder) =>
-      disposeHolder.bindDisposable(this, dispose: cancel);
+      disposeHolder.bindDisposable(this, dispose: (_) => cancel());
 }
 
 extension DisposableExtension<D extends Disposable> on D {
   D bindDisposeTo(CustomDisposableCallHost disposeHolder) =>
-      disposeHolder.bindDisposable(this, dispose: dispose);
+      disposeHolder.bindDisposable(this, dispose: (_) => dispose());
 }
