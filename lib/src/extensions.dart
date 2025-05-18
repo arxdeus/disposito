8a1a3776f6deb0b11589ddd95744111367a13124
Artemis Kushner <git@arxdeus.dev>
2025-05-18 03:12:13 +0300

    docs: add comprehensive doc comments for classes
    This commit adds:
    - Full documentation for `Disposable`, `Closable`, and `Cancelable` interfaces
    - Detailed comments for extension methods (bindDisposeTo) in:
      - `Sink<T>` extension
      - `ClosableExtension`
      - `DisposableExtension`
    - Documentation for `DisposeRegistry` `purgeAfter` method
    - Comments for internal classes: `DisposeGroup`, `NamedObject`, `DisposableBinderHost`
    - Doc comments for properties/methods in `DisposeHolderHostMixin`
    - Comprehensive documentation for the `Callable` abstract class

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -3,37 +3,44 @@ import 'dart:async';
 import 'package:disposito/src/interfaces.dart';
 import 'package:disposito/src/internal/internal.dart';
 
+/// Extension for StreamSubscription objects to bind disposal to a DisposableBinderHost.
 extension StreamSubscrptionExtensions<T, S extends StreamSubscription<T>> on S {
+  /// Bind subscription disposal to the given host.
   S bindDisposeTo(DisposableBinderHost disposeHolder) =>
       disposeHolder.bindDisposable(this, dispose: (_) => cancel());
 }
 
+/// Extension for StreamController objects to bind disposal to a DisposableBinderHost.
 extension StreamControllerExtensions<T, S extends StreamController<T>> on S {
+  /// Bind controller disposal to the given host.
   S bindDisposeTo(DisposableBinderHost disposeHolder) =>
       disposeHolder.bindDisposable(this, dispose: (_) => close());
 }
 
+/// Extension for Timer objects to bind disposal to a DisposableBinderHost.
 extension TimerExtensions on Timer {
+  /// Bind timer disposal to the given host.
   Timer bindDisposeTo(DisposableBinderHost disposeHolder) =>
       disposeHolder.bindDisposable(this, dispose: (_) => cancel());
 }
 
+/// Extension for Sink objects to bind disposal to a DisposableBinderHost.
 extension SinkExtension<T, S extends Sink<T>> on S {
+  /// Bind sink disposal to the given host.
   S bindDisposeTo(DisposableBinderHost disposeHolder) =>
       disposeHolder.bindDisposable(this, dispose: (_) => close());
 }
 
+/// Extension for Closable objects to bind disposal to a DisposableBinderHost.
 extension ClosableExtension<C extends Closable> on C {
+  /// Bind closable object disposal to the given host.
   C bindDisposeTo(DisposableBinderHost disposeHolder) =>
       disposeHolder.bindDisposable(this, dispose: (_) => close());
 }
 
-extension CancelableExtension<C extends Cancelable> on C {
-  C bindDisposeTo(DisposableBinderHost disposeHolder) =>
-      disposeHolder.bindDisposable(this, dispose: (_) => cancel());
-}
-
+/// Extension for Disposable objects to bind disposal to a DisposableBinderHost.
 extension DisposableExtension<D extends Disposable> on D {
+  /// Bind disposable object disposal to the given host.
   D bindDisposeTo(DisposableBinderHost disposeHolder) =>
       disposeHolder.bindDisposable(this, dispose: (_) => dispose());
 }
